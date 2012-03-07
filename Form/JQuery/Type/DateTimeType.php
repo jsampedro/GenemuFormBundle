f99e8c2138e9374658201360999a33353da40db4
jsampedro <javier@sampedrobcn.es>
2012-03-07 20:04:11 +0100

    datetimepicker as text field

--- Form/JQuery/Type/DateTimeType.php
+++ Form/JQuery/Type/DateTimeType.php
@@ -1,22 +1,84 @@
 <?php
+
 namespace Genemu\Bundle\FormBundle\Form\JQuery\Type;
 
-use Genemu\Bundle\FormBundle\Form\JQuery\Type\DateType as BaseType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 
 /**
- * DateTimeType
+ * DateType
  *
  */
-class DateTimeType extends BaseType
-{
+class DateTimeType extends AbstractType {
+
+    private $options;
+
+    /**
+     * Constructs
+     *
+     * @param array $options
+     */
+    public function __construct(array $options) {
+        $this->options = $options;
+    }
 
     /**
      * {@inheritdoc}
      */
-    public function getName()
-    {
+    public function buildForm(FormBuilder $builder, array $options) {
+        $options = $this->getDefaultOptions($options);
+
+        $builder
+                ->setAttribute('culture', $options['culture'])
+                ->setAttribute('configs', $options['configs']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form) {
+        $configs = $form->getAttribute('configs');
+    
+
+        $view
+                ->set('configs', $configs)
+                ->set('culture', $form->getAttribute('culture'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultOptions(array $options) {
+        $defaultOptions = array(
+            'culture' => \Locale::getDefault(),
+            'input' => 'datetime',
+            'date_widget' => 'single_text',
+            'time_widget' => 'single_text',
+            'configs' => array_merge(array(
+                'dateFormat' => 'dd-mm-yy',
+                    ), $this->options),
+        );
+
+        $options = array_replace_recursive($defaultOptions, $options);
+
+        return $options;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent(array $options) {
+        return 'text';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName() {
         return 'genemu_jquerydatetime';
     }
 
- 
+
 }
